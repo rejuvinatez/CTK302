1a584a8a66d12be89322f878a27baa5426420156
rejuvinatez <dyanbermeo@gmail.com>
2020-09-10 00:02:21 -0500

    Programming sucks

---
+++ p5/Unit1/mp1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background('grey');
+    if (mouseIsPressed) {
+    background('red');
+  }
+
+    
+ellipse(100, 100, 100, 100);
+}

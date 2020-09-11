40e9e37e16f63bded6c42945303c1a6f12ff3481
rejuvinatez <dyanbermeo@gmail.com>
2020-09-10 20:57:33 -0500

    Superheroes

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -1,13 +1,61 @@
-function setup() {
-  createCanvas(500, 500);
-}
+
+//Dyan Bermeo, Fall 2020
+var f;
+
+ function setup() {
+   createCanvas(800, 800);
+
+   f = loadFont("assets/GoudyTrajanBold.ttf");
+
+
+
+
+ }
 
 function draw() {
-  background('grey');
-    if (mouseIsPressed) {
-    background('red');
+  background('#FA8072');
+
+if (mouseIsPressed) {
+
+
+  fill('black');
+  textFont(f);
+  textSize(30);
+  text('Dyno is a sneaky villain who is hard to find. He can be really quiet when moving and stealthy',40, 600, 600, 200) ;
+
+    fill('red');
+  ellipse(250,250,500,500);
+   fill('orange')
+  ellipse(250,250,400,400);
+
   }
 
-    
-ellipse(100, 100, 100, 100);
+// Palm hands
+fill('#EE3E38 ');
+noStroke();
+rect(150, 180, 180,280);
+
+  // Middle Fingers
+  fill ('#FFD0C2');
+  noStroke();
+  rect(210, 80, 40, 100);
+
+  // Sparks
+  strokeWeight(1);
+stroke(51);
+  fill ('#FFA500');
+
+  ellipse(230, 100, 60, 60);
+
+
+  //
+  fill("black");
+  noStroke();
+  rect(195, 180, 70, 25);
+
+
+
+
+
+
 }

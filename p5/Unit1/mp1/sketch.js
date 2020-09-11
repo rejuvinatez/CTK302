3da2780b07c6d1915892f97b7695ffc8d8941b9a
rejuvinatez <dyanbermeo@gmail.com>
2020-09-10 21:19:45 -0500

    Heroeis final

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -5,7 +5,7 @@ var f;
  function setup() {
    createCanvas(800, 800);
 
-   f = loadFont("assets/GoudyTrajanBold.ttf");
+   f = loadFont("/assets/GoudyTrajanBold.ttf");
 
 
 
@@ -20,8 +20,8 @@ if (mouseIsPressed) {
 
   fill('black');
   textFont(f);
-  textSize(30);
-  text('Dyno is a sneaky villain who is hard to find. He can be really quiet when moving and stealthy,',40, 600, 600, 200) ;
+  textSize(60);
+  text('Dyno is a sneaky villain who is hard to find. He can be really quiet when moving and stealthy',40, 600) ;
 
     fill('red');
   ellipse(250,250,500,500);
@@ -53,7 +53,52 @@ stroke(51);
   noStroke();
   rect(195, 180, 70, 25);
 
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+    fill('white');
+  rect(195, 200, 70, 25);
 
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+    fill('black');
+  rect(195, 220, 70, 25);
+
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+    fill('white');
+  rect(195, 240, 70, 25);
+
+    // Draw a rectangle at location (30, 20) with a width and height of 55.
+    fill('black');
+  rect(195, 260, 70, 25);
+
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('white');
+  rect(195, 280, 70, 25);
+
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('black');
+  rect(195, 290, 70, 25);
+
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('white');
+  rect(195, 310, 70, 25);
+
+
+  // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('black');
+  rect(195, 330, 70, 25);
+
+
+   // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('white');
+  rect(195, 350, 70, 25);
+
+
+   // Draw a rectangle at location (30, 20) with a width and height of 55.
+  fill('white');
+  rect(195, 390, 70, 25);
+
+  arc(210, 80, 50, 50, 0, HALF_PI);
+noFill();
+arc(50, 55, 60, 60, HALF_PI, PI);
 
 
 

1a8ff9c4efb2e93a9ade59a02bc67d50f6485a89
rejuvinatez <dyanbermeo@gmail.com>
2020-10-18 19:13:21 -0500

    stoplight

---
+++ p5/Unit2/Stoplight/sketch.js
@@ -0,0 +1,73 @@
+let state = 0;
+let timer = 0;
+let x = 0;
+let velocity = 10;
+
+function setup() {
+  createCanvas(800, 800);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+}
+
+function draw() {
+
+background(100);
+fill('yellow');
+rect(width / 2, height / 2, 200, 700);
+
+// car code
+fill ('blue');
+rect(0, 750, 75, 70);
+x = x + velocity;
+if (x >width)  {
+  x = 0;
+}
+
+
+switch (state) {
+
+  case 0: //red
+  text("0", 100, 100);
+  fill('red');
+  ellipse(width / 2, height / 2 - 170, 150, 150);
+  fill('grey');
+  ellipse(width / 2, height / 2, 150, 150);
+  ellipse(width / 2, height /2 + 170, 150, 150);
+  velocity = 0;
+  break;
+
+  case 1: // yellow
+  text("1", 100, 100);
+  fill('grey');
+  ellipse(width / 2, height / 2 - 170, 150, 150);
+  ellipse(width / 2, height / 2, 150, 150);
+fill('green');
+  ellipse(width / 2, height /2 + 170, 150, 150);
+  velocity = 10;
+  break;
+
+  case 2: // yellow
+  text("2", 100, 100);
+  fill('grey');
+  ellipse(width / 2, height / 2 - 170, 150, 150);
+  fill('yellow');
+  ellipse(width / 2, height / 2, 150, 150);
+  fill('grey');
+  ellipse(width / 2, height /2 + 170, 150, 150);
+  velocity = 05;
+  break;
+}
+
+timer = timer + 1;
+if (timer > 100) {
+  timer = 0;
+  state++;
+  if (state >2) state = 0;
+ }
+}
+
+function mouseReleased(){
+  state++;
+  if (state >2) state =0 ;
+
+}

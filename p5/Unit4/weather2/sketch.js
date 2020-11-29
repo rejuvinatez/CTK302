14c11f2e2c7f0d57c5ea7e0fa1454055987ce328
rejuvinatez <dyanbermeo@gmail.com>
2020-11-29 11:32:45 -0600

    weather

---
+++ p5/Unit4/weather2/sketch.js
@@ -0,0 +1,80 @@
+var weather;
+var weatherID = 0; // returned in the JSON weather element
+var myState = 0;
+var x = 0;
+var windspeed = 0;
+var temperature = 0;
+var humidity = 0;
+var name = '';
+let img;
+
+
+function setup() {
+  createCanvas(400, 400);
+
+  // HERE is the call to get the weather.
+
+  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Chicago,IL,US&units=imperial&';
+
+  //You can also use "zipcode" - var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61820,us&units=imperial&';
+
+  var myIDString = 'appid=1c6e8464ca585cef83b2bf0197960c34'; // USE YOUR ID HERE, take out the x's!!!
+
+  var myBigString = myCityString + myIDString;
+
+  loadJSON(myBigString, gotData); // that gotData function happens when JSON comes back.
+
+  // here we use a callback to display the image after loading
+ loadImage('assets/chicago.png', img => {
+   image(img, 100, 110);
+ });
+
+}
+
+
+function gotData(data) {
+
+  weather = data;
+  console.log(weather); // for debugging purposes, print out the JSON data when we get it.
+  windspeed = weather.wind.speed;
+  temperature = weather.main.temp;
+  humidity = weather.main.humidity;
+  name = weather.name;
+
+
+}
+
+
+function draw() {
+  switch (myState) {
+    case 0:
+      if (weather) {
+        myState = 1;
+      }
+      break;
+
+    case 1:
+
+      //
+      background('blue');
+      fill('yellow');
+      text("What is the weather in " + weather.name + "?", 20, 20);
+      text("windspeed is " + windspeed, 20, 40);
+      text("humidity is " + humidity, 20, 60);
+      text("temperature is " + temperature, 20, 80);
+
+
+      // cloud
+      fill('white');
+      noStroke();
+      ellipse(x, 300, 200, 100);
+      ellipse(x + 50, 350, 300, 70);
+
+      // move the cloud's x position
+      x = x + windspeed / 4;
+      if (x > width) x = 0;
+
+      break;
+
+  }
+}
